e5a9881a6225ce4014393c50a06ef14c734f5f0b
John Ballesteros <johnshift.dev@gmail.com>
2021-09-07 19:08:01 +0800

    basic redux store setup

---
+++ src/components/Profile.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Profile() {
+  return (
+    <div>
+      <h1>Profile Page</h1>
+      <p>name: </p>
+      <p>age: </p>
+      <p>email: </p>
+    </div>
+  );
+}
+
+export default Profile;
